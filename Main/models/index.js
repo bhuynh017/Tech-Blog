5284ab03beb3a4d5dc8a21ad26c82d245099565a
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-03 11:35:25 -0600

    adding code to index.js in the models folder. Creating the connection

---
+++ Main/models/index.js
@@ -0,0 +1,4 @@
+// creating these lines of code to be able to utilize other code from JavaScript files located in the models folder.
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
