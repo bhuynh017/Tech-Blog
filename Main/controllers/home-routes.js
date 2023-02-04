b2a623a6f1033c4295fa365afd6d5182712c2a31
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 12:06:19 -0600

    adding the first GET route in order to retrieve all posts from the homepage

--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -1,2 +1,20 @@
 const router = require('express').Router();
 const { Post, Comment, User } = require('../models/');
+
+
+// defining a GET route to get posts from the homepage.
+router.get('/', async (req, res) => {
+    // this "try" is retrieving all posts from the db using the findAll method
+    try {
+      const postData = await Post.findAll({
+        // including the user with the post
+        include: [User],
+      });
+  
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      res.render('all-posts', { posts });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
