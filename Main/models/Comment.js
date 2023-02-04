2e4d17ae6b2e28af4d8369ac7eb30f3a6c0d63fb
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 11:25:10 -0600

    adding models into the Comment.js

--- Main/models/Comment.js
+++ Main/models/Comment.js
@@ -2,3 +2,20 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 // importing the database config
 const sequelize = require('../config/config');
+
+class Comment extends Model {}
+
+// creating a new model called Comment containing one body which is a string.
+Comment.init(
+  {
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
+    sequelize
+  }
+);
+
+module.exports = Comment;
