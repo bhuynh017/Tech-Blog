58cf63c2b97d5ca81483cf4e32f954d564475b48
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 19:20:46 -0600

    adding delete route

--- Main/controllers/api/post-routes.js
+++ Main/controllers/api/post-routes.js
@@ -34,3 +34,24 @@ router.post('/', withAuth, async (req, res) => {
       res.status(500).json(err);
     }
   });
+
+  // delete a post by using Post.destroy.
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const [affectedRows] = Post.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  module.exports = router;
