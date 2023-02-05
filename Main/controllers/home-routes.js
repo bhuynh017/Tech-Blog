a73c75c1b44316bd4b21f27a0b519775400e38fb
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 18:32:41 -0600

    completing home-routes by adding login and sign up get routes

--- Main/controllers/home-routes.js
+++ Main/controllers/home-routes.js
@@ -1,45 +1,64 @@
-const router = require('express').Router();
-const { Post, Comment, User } = require('../models/');
-
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models/");
 
 // defining a GET route to get posts from the homepage.
-router.get('/', async (req, res) => {
-    // this "try" is retrieving all posts from the db using the findAll method
-    try {
-      const postData = await Post.findAll({
-        // including the user with the post
-        include: [User],
-      });
-  
-      const posts = postData.map((post) => post.get({ plain: true }));
-  
-      res.render('all-posts', { posts });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/", async (req, res) => {
+  // this "try" is retrieving all posts from the db using the findAll method
+  try {
+    const postData = await Post.findAll({
+      // including the user with the post
+      include: [User],
+    });
 
-  // this is retrieving a single post.
-  router.get('/post/:id', async (req, res) => {
-    try {
-      const postData = await Post.findByPk(req.params.id, {
-        include: [
-          User,
-          {
-            model: Comment,
-            include: [User],
-          },
-        ],
-      });
-  
-      if (postData) {
-        const post = postData.get({ plain: true });
-  
-        res.render('single-post', { post });
-      } else {
-        res.status(404).end();
-      }
-    } catch (err) {
-      res.status(500).json(err);
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("all-posts", { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// this is retrieving a single post.
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+
+      res.render("single-post", { post });
+    } else {
+      res.status(404).end();
     }
-  });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
+module.exports = router;
--- Main/controllers/index.js
+++ Main/controllers/index.js
@@ -1,3 +1,4 @@
+// setting up routes
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
