985a84a2ae4e92db0e34d27844afc37d4cca1dcc
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 19:23:53 -0600

    updating config connection route

--- Main/models/Comment.js
+++ Main/models/Comment.js
@@ -1,7 +1,7 @@
 // importing the modules 
 const { Sequelize, Model, DataTypes } = require('sequelize');
 // importing the database config
-const sequelize = require('../config/config');
+const sequelize = require('../config/connection');
 
 class Comment extends Model {}
 
