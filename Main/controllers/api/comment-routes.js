63c9c2231d808331c50875349d392efe866a9599
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 18:43:07 -0600

    adding route handler

--- Main/controllers/api/comment-routes.js
+++ Main/controllers/api/comment-routes.js
@@ -2,4 +2,17 @@ const router = require('express').Router();
 const { Comment } = require('../../models/');
 const withAuth = require('../../utils/auth');
 
-
+// withAuth ensuring the user is authenticated before they can access the route.
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body,
+        userId: req.session.userId,
+      });
+      res.json(newComment);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
