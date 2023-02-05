d95a9e7a1a8ad11931cbe72f452483581c57c713
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-05 14:43:28 -0600

    adding const

--- Main/public/js/login.js
+++ Main/public/js/login.js
@@ -1,5 +1,8 @@
 const loginHandler = async function(event) {
     event.preventDefault();
   
+    const usernameEl = document.querySelector('#username-input-login');
+    const passwordEl = document.querySelector('#password-input-login');
+  
 }
   
