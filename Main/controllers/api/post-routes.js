b9526efad1a09b6bbb7d7917f0cb348df95e75ae
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-04 19:19:08 -0600

    adding post and put routes to allow access to post-routes.js

--- Main/controllers/api/post-routes.js
+++ Main/controllers/api/post-routes.js
@@ -1,3 +1,36 @@
 const router = require('express').Router();
 const { Post } = require('../../models/');
 const withAuth = require('../../utils/auth');
+
+// Setting up single route. withAuth is ensuring user is authenticated before they can access the route.
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+
+    // creating new post and passing it over an object.
+    try {
+      const newPost = await Post.create({ ...body, userId: req.session.userId });
+      res.json(newPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.put('/:id', withAuth, async (req, res) => {
+    try {
+        // updating a post.
+      const [affectedRows] = await Post.update(req.body, {
+        // finds the post and id
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (affectedRows > 0) {
+        res.status(200).end();
+      } else {
+        res.status(404).end();
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
