659c70539f9274064680248b62eadd65fd4c59b3
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-05 14:43:50 -0600

    adding response and post method

--- Main/public/js/login.js
+++ Main/public/js/login.js
@@ -4,5 +4,22 @@ const loginHandler = async function(event) {
     const usernameEl = document.querySelector('#username-input-login');
     const passwordEl = document.querySelector('#password-input-login');
   
-}
-  
+    const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+          username: usernameEl.value,
+          password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+    
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Login Failure');
+      }
+    };
+    
+    document
+      .querySelector('#login-form')
+      .addEventListener('submit', loginHandler);
